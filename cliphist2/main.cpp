9bb47d74287fbab93ffbf0b8c1bc88469f6c53fb
min <min@3dae2a8a-bb7e-c243-a14e-55e8ab72d081>
2010-01-06 09:13:31 +0000

    added header infos (project and license)
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_cliphist2/trunk@19 3dae2a8a-bb7e-c243-a14e-55e8ab72d081

--- cliphist2/main.cpp
+++ cliphist2/main.cpp
@@ -1,3 +1,27 @@
+/**************************************************************************
+ *
+ *	project				 : cliphist2
+ *
+ *	copyright            : (C) 2009-2010 by Michael Neuroth
+ *
+ * ------------------------------------------------------------------------
+ *
+ *  $Source: E:/home/cvsroot/visiscript/visiscript.cpp,v $
+ *
+ */
+/*********************************************************************************
+ *																				 *
+ * This file is part of the Cliphist2 package (a clipboard history application)  *
+ *																				 *
+ * Copyright (C) 2009-2010 by Michael Neuroth.								     *
+ *                                                                               *
+ * This program is free software; you can redistribute it and/or modify			 *
+ * it under the terms of the GNU General Public License as published by    		 *
+ * the Free Software Foundation; either version 2 of the License, or             *
+ * (at your option) any later version.                                           *
+ *                                                                               *
+ ********************************************************************************/
+
 #include <QtGui/QApplication>
 #include <QTranslator>
 #include <QLocale>
