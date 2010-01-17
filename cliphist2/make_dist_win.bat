9b5c0bad6ea427012431d88ab89d452c0cae546b
min <min@3dae2a8a-bb7e-c243-a14e-55e8ab72d081>
2010-01-17 13:19:52 +0000

    improved generation of windows binary distribution: remove dependencies
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_cliphist2/trunk@74 3dae2a8a-bb7e-c243-a14e-55e8ab72d081

--- cliphist2/make_dist_win.bat
+++ cliphist2/make_dist_win.bat
@@ -3,5 +3,8 @@ echo "call qmake"
 qmake -r cliphist2.pro CONFIG+=release
 echo "make app"
 mingw32-make Release
+echo "compress executable"
+upx -9 release\cliphist2.exe
 echo "call Inno-Setup to create setup file"
-"C:\Programme\Inno Setup 5\iscc" dist\cliphist2.iss /Fcliphist2_setup /O./dist
+"C:\Programme\Inno Setup 5\iscc" dist\cliphist2_static.iss /Fcliphist2_setup /O./dist
+rem "C:\Programme\Inno Setup 5\iscc" dist\cliphist2.iss /Fcliphist2_setup /O./dist
