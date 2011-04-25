265b8ac34c4ce08653784d7ef4dab68dffca3e21
min <min@3dae2a8a-bb7e-c243-a14e-55e8ab72d081>
2011-04-25 10:09:39 +0000

    updated for first public release
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_cliphist2/trunk@112 3dae2a8a-bb7e-c243-a14e-55e8ab72d081

--- cliphist2/make_src_tar.sh
+++ cliphist2/make_src_tar.sh
@@ -3,5 +3,5 @@ mkdir cliphist2-1.0.0
 #./do_file_list.sh
 tar -cf - -T src_files.lst | tar -xpf - -C cliphist2-1.0.0
 tar -z -cf cliphist2-1.0.0.tar.gz cliphist2-1.0.0/*
-#rm -rf cliphist2-1.0.0/
+rm -rf cliphist2-1.0.0/
 #list: tar -tvf cliphist2-1.0.0.tar.gz
