0b3c5c1fefed3071af6f5ff01a26141c139974bc
min <min@3dae2a8a-bb7e-c243-a14e-55e8ab72d081>
2012-01-21 14:55:59 +0000

    improved build script for binary distribution for Mac
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_cliphist2/trunk@121 3dae2a8a-bb7e-c243-a14e-55e8ab72d081

--- cliphist2/make_dist_mac.sh
+++ cliphist2/make_dist_mac.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 echo "make distribution of cliphist2 for Mac OS X (*.app and *.dmg):"
-#cp /Developer/Applications/Qt/translations/qt_de.qm .
-cp /Users/min/Downloads/Programmierung/qt-everywhere-opensource-src-4.7.3/translations/qt_de.qm .
+echo "using QT distribution in directory: " $QT_DIR
+cp $QT_DIR/translations/qt_de.qm .
 echo "call qmake"
 qmake -spec macx-g++ cliphist2.pro CONFIG+=release CONFIG-=debug
 echo "make app"
 make
-cp -r /Users/min/Downloads/Programmierung/qt-everywhere-opensource-src-4.7.3/src/gui/mac/qt_menu.nib cliphist2.app/Contents/Resources
-cp cliphist2orig.clp cliphist2.app/Contents/Resources/cliphist2.clp
+# workaround for qt_menu.nib (see: https://bugreports.qt.nokia.com//browse/QTBUG-5952)
+cp -r $QT_SRC_DIR/src/gui/mac/qt_menu.nib cliphist2.app/Contents/Resources
+cp cliphist2.clp cliphist2.app/Contents/Resources/cliphist2.clp
 echo "make dmg"
 rm cliphist2.dmg
-macdeployqt cliphist2.app -dmg -verbose=3 -no-plugins
+$QT_DIR/bin/macdeployqt cliphist2.app -dmg -verbose=3 -no-plugins
 echo "clean up"
 #rm qt_de.qm
