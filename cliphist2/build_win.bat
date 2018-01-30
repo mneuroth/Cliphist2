c30f46c7818aff57b44cbb9ebbe0039fa8de392c
Michael Neuroth <michael.neuroth@freenet.de>
2018-01-30 22:08:35 +0100

    Added steps to create binary installer with qt installer framework

--- cliphist2/build_win.bat
+++ cliphist2/build_win.bat
@@ -3,7 +3,9 @@ qmake -r cliphist2.pro
 nmake clean
 nmake release
 cd release
-mkdir deploy
-copy cliphist2.exe deploy
-cd deploy
+mkdir ..\dist\packages\de.mneuroth.cliphist2\data
+copy cliphist2.exe ..\dist\packages\de.mneuroth.cliphist2\data
+cd ..\dist\packages\de.mneuroth.cliphist2\data
 windeployqt --release --no-system-d3d-compiler cliphist2.exe
+cd ..\..\..
+binarycreator -c config\config.xml -p packages Cliphist2Installer.exe
